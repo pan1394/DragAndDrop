3e1f178b74e0106e1352002df61b6eaddfd1af40
panyl <pan1394@126.com>
2019-03-07 10:06:24 +0800

    dd

--- com.linkage.dnd.demo/src/com/linkage/dnd/data/Constants.java
+++ com.linkage.dnd.demo/src/com/linkage/dnd/data/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
 	public static final String PREFIX_JAVA_OBJECT= "JAVA_OBJECT_";
 	
-	public static final String PREFIX_TEXT= "TEXT_asdf";
+	public static final String PREFIX_TEXT= "TEXT_";
 	
 	public static final String TEMPLATE = "传递了一个Java{%s}对象到%s组件%s内";
 	
